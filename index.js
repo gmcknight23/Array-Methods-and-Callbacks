ad1ad302cccaa1b4be8cb48c741cd797da4f077a
gregmcknight <gmcknight23@gmail.com>
2022-01-16 22:21:54 -0500

    first questions

--- index.js
+++ index.js
@@ -5,13 +5,33 @@ const { fifaData } = require('./fifa.js')
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
 
+// const finals2014 = fifaData.filter(function(item){
+//     return item.Year === 2014 && item.Stage === 'Final';
+// });
+
+// console.log(finals2014)
+
+const finals2014 = fifaData.filter((item) => {
+    return item.Year === 2014 && item.Stage === 'Final';
+});
+
+console.log(finals2014)
+
 //(a) Home Team name for 2014 world cup final
+
+console.log(finals2014[0]['Home Team Name']);
 
 //(b) Away Team name for 2014 world cup final
 
+console.log(finals2014[0]['Away Team Name']);
+
 //(c) Home Team goals for 2014 world cup final
 
+console.log(finals2014[0]['Home Team Goals']);
+
 //(d) Away Team goals for 2014 world cup final
+
+console.log(finals2014[0]['Away Team Goals']);
 
 //(e) Winner of 2014 world cup final */
 
